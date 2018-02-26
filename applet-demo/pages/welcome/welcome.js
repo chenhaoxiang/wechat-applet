1e854b4b770d33bf1f85bbc6852393a882bfa028
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-02-26 22:25:47 +0800

    页面跳转与冒泡事件

--- applet-demo/pages/welcome/welcome.js
+++ applet-demo/pages/welcome/welcome.js
@@ -7,6 +7,25 @@ Page({
     
   },
 
+//向上冒泡事件 bind事件绑定不会阻止冒泡事件向上冒泡，catch事件绑定可以阻止冒泡事件向上冒泡。
+  onTextTap: function (event){
+    console.log("onTextTap");
+  },
+  onTap:function(event){
+    console.log("onTop");
+    //页面跳转
+    //父级跳往子级页面，左上角有返回按钮。层级关系最多只能有5层
+    // wx.navigateTo({
+    //   url: '../posts/post',
+    // });//会执行onHide函数 
+
+// 跳转页面后，左上角没有返回
+    wx.redirectTo({
+      url: '../posts/post',
+    });//会执行onUnload函数  页面被关闭了
+
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -29,17 +48,17 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * 生命周期函数--监听页面隐藏 关闭
    */
   onHide: function () {
-    
+    console.log("onHide");
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+    console.log("onUnload");
   },
 
   /**
